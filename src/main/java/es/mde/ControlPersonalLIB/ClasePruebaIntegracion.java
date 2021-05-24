fe5e7f622ac145357f6acd49a61123b81e25d574
jlportus <jl_puerto@hotmail.com>
2021-05-24 09:00:42 +0200

    Iniciando Poryecto Libreria

---
+++ src/main/java/es/mde/ControlPersonalLIB/ClasePruebaIntegracion.java
@@ -0,0 +1,8 @@
+package es.mde.ControlPersonalLIB;
+
+public class ClasePruebaIntegracion {
+
+	public static void imprime() {
+		System.out.println("Estoy en la libreria");
+	}
+}
--- src/main/java/es/mde/ControlPersonalLIB/ControlPersonalLibApplication.java
+++ src/main/java/es/mde/ControlPersonalLIB/ControlPersonalLibApplication.java
@@ -1,13 +1,10 @@
 package es.mde.ControlPersonalLIB;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class ControlPersonalLibApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ControlPersonalLibApplication.class, args);
+		
 	}
 
 }
