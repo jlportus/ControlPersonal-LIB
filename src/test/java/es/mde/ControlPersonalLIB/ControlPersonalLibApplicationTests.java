57296bd3703b9c89b74119d59f9597ef46000181
jlportus <jl_puerto@hotmail.com>
2021-05-24 08:53:35 +0200

    first commit

---
+++ src/test/java/es/mde/ControlPersonalLIB/ControlPersonalLibApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mde.ControlPersonalLIB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlPersonalLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
